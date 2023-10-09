e78d044ce9ab961e83b6a817b0a4b353eac41684
Svereda <you@example.com>
2023-10-09 23:26:11 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'; // Додайте Navigate
 import Pubs from './pages/pubs';
 import Forum from "./pages/forum";
 import Vhid from "./pages/login";
@@ -7,7 +7,6 @@ import Golovna from "./pages/golovna";
 import './styles.css';
 import logotyp from './image/logo.png'
 import login from './image/login.png'
-
 
 function App() {
   return (
@@ -22,25 +21,25 @@ function App() {
 
               <div className="nav">
                 <ul>
-                  <Link className = "nav-item" to="/golovna">ГОГОВНА</Link>
-                  <Link className = "nav-item" to="/pubs">ПАБИ</Link>
-                  <Link className = "nav-item" to="/forum">ФОРУМ</Link>
+                  <Link className="nav-item" to="/golovna">ГОЛОВНА</Link>
+                  <Link className="nav-item" to="/pubs">ПАБИ</Link>
+                  <Link className="nav-item" to="/forum">ФОРУМ</Link>
                 </ul>
               </div>
 
               <div className="card">
-                <Link className = "nav-item" to="/login"><img src={login} alt="Логотип" className="logo" /></Link>
+                <Link className="nav-item" to="/login"><img src={login} alt="Логотип" className="logo" /></Link>
               </div>
             </div>
           </div>
         </div>
-        
 
         <Routes>
           <Route path="/pubs" element={<Pubs />} />
           <Route path="/forum" element={<Forum />} />
           <Route path="/login" element={<Vhid />} />
-          <Route path="/golovna" element={<Golovna/>} />
+          <Route path="/golovna" element={<Golovna />} />
+          <Route path="/" element={<Navigate to="/golovna" />} />
         </Routes>
       </div>
     </Router>
